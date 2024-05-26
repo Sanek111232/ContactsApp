8b762dcf071fe4b9cd7ba15e9c692d51fa2ed4c4
Sanek111232 <am8220516@gmail.com>
2024-05-26 12:55:04 +0700

    Были добавлены формы: ContactsAppForm, AddEditForm, AboutProgramForm

---
+++ ContactsAppUI/AddEditContactForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ContactsAppUI
+{
+    partial class AddEditContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.VKTextBox = new System.Windows.Forms.TextBox();
+            this.BirthdayDateTime = new System.Windows.Forms.DateTimePicker();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.NameTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.SurnameTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.PhoneTextBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.EmailTextBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.VKTextBox, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.BirthdayDateTime, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(592, 174);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Surname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Birthday:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "E-mail:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "vk.com:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(151, 3);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(438, 22);
+            this.NameTextBox.TabIndex = 6;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(151, 31);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(438, 22);
+            this.SurnameTextBox.TabIndex = 7;
+            // 
+            // PhoneTextBox
+            // 
+            this.PhoneTextBox.Location = new System.Drawing.Point(151, 87);
+            this.PhoneTextBox.Name = "PhoneTextBox";
+            this.PhoneTextBox.Size = new System.Drawing.Size(438, 22);
+            this.PhoneTextBox.TabIndex = 8;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(151, 115);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(438, 22);
+            this.EmailTextBox.TabIndex = 9;
+            // 
+            // VKTextBox
+            // 
+            this.VKTextBox.Location = new System.Drawing.Point(151, 143);
+            this.VKTextBox.Name = "VKTextBox";
+            this.VKTextBox.Size = new System.Drawing.Size(438, 22);
+            this.VKTextBox.TabIndex = 10;
+            // 
+            // BirthdayDateTime
+            // 
+            this.BirthdayDateTime.Location = new System.Drawing.Point(151, 59);
+            this.BirthdayDateTime.Name = "BirthdayDateTime";
+            this.BirthdayDateTime.Size = new System.Drawing.Size(200, 22);
+            this.BirthdayDateTime.TabIndex = 11;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(529, 192);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Отмена";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(448, 192);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmButton.TabIndex = 2;
+            this.ConfirmButton.Text = "OK";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // AddEditContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 223);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AddEditContactForm";
+            this.Text = "Add/Edit Contact";
+            this.Shown += new System.EventHandler(this.AddEditContactForm_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.TextBox PhoneTextBox;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox VKTextBox;
+        private System.Windows.Forms.DateTimePicker BirthdayDateTime;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button ConfirmButton;
+    }
+}
---
+++ ContactsAppUI/ContactsAppForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ContactsApp.Project;
+using System.Windows.Forms;
+using ContactsApp;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ContactsAppUI
+{
+    /// <summary>
+    /// Основная форма приложения
+    /// </summary>
+    public partial class ContactsAppForm : Form
+    {
+        Project _contactsProject;
+        /// <summary>
+        /// Список всех контактов
+        /// </summary>
+        public Project ContactsProject
+        {
+            get
+            { return _contactsProject; }
+            set
+            { _contactsProject = value; }
+        }
+        public ContactsAppForm()
+        {
+            InitializeComponent();
+            ContactsProject = ProjectManager.LoadFromFile();
+            FindTextBox.Text = "";
+            if (ContactsProject == null)
+            {
+                PhoneNumber phone = new PhoneNumber(78901234567);
+                ContactsProject = new Project(new List<Contact>() { { new Contact("Меньшиков", "Александр", "m@mail.ru", "id777", DateTime.Now, phone) } });
+            }
+            else
+            {
+                var contact = ContactsProject.CurrentContact;
+                if (contact == null)
+                {
+                    NameTextBox.Text = "";
+                    SurnameTextBox.Text = "";
+                    EmailTextBox.Text = "@";
+                    VKTextBox.Text = "";
+                    BirthdayDateTime.Text = DateTime.Now.ToString();
+                    PhoneTextBox.Text = 78005553535.ToString();
+                }
+                else
+                {
+                    NameTextBox.Text = contact.Name;
+                    SurnameTextBox.Text = contact.Surname;
+                    EmailTextBox.Text = contact.Email;
+                    VKTextBox.Text = contact.IdVK;
+                    BirthdayDateTime.Text = contact.BirthDate.ToString();
+                    PhoneTextBox.Text = contact.ContactNumber.Number.ToString();
+                }
+            }
+            List<Contact> contactsWithBirthday = ContactsProject.GetContactsWithBirthday(DateTime.Now);
+            if (contactsWithBirthday.Count == 0)
+            {
+                birthdayListRichTextBox.Text = "Сегодня нет контактов с днем рождения";
+            }
+            else
+            {
+                birthdayListRichTextBox.Text = "Сегодня день рождения:\n";
+                foreach (var conact in contactsWithBirthday)
+                {
+                    birthdayListRichTextBox.Text += conact.Surname + " " + conact.Name + "\n";
+                }
+            }
+            CreateContactList();
+        }
+        /// <summary>
+        /// Пересоздаёт лист со всеми контактами
+        /// </summary>
+        /// <param name="defaultSelectedIndex">номер контакта, который будет выделен после пересоздания</param>
+        void RecreateContactList(string selectedName)
+        {
+            List<Contact> contacts = new List<Contact>();
+            contacts = ContactsProject.GetSortedContacts(selectedName);
+            ContactsListBox.Items.Clear();
+            foreach (var contact in contacts)
+            {
+                ContactsListBox.Items.Add(contact.Surname + " " + contact.Name);
+            }
+        }
+        void CreateContactList()
+        {
+            List<Contact> contacts = ContactsProject.Contacts;
+            contacts.Sort(new ContactComparer());
+            ContactsListBox.Items.Clear();
+            foreach (var contact in contacts)
+            {
+                ContactsListBox.Items.Add(contact.Surname + " " + contact.Name);
+            }
+        }
+        /// <summary>
+        /// Обработка кнопки "Добавить контакт"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddContactButton_Click(object sender, EventArgs e)
+        {
+            AddEditContactForm addEditContactForm = new AddEditContactForm();
+            var dialogResult = addEditContactForm.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                Contact contact = addEditContactForm.CurrentContact;
+                ContactsProject.Contacts.Add(contact);
+                CreateContactList();
+                ProjectManager.SaveToFile(ContactsProject);
+            }
+        }
+
+        /// <summary>
+        /// Обработка кнопки "Редактировать контакт"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditContactButton_Click(object sender, EventArgs e)
+        {
+            AddEditContactForm addEditContactForm = new AddEditContactForm();
+            Contact contact = ContactsProject.Contacts[ContactsListBox.SelectedIndex];
+            addEditContactForm.CurrentContact = contact;
+            var dialogResult = addEditContactForm.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                CreateContactList();
+            }
+        }
+        /// <summary>
+        /// Обработка кнопки "Удалить контакт"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RemoveContactButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены, что хотите удалить контакт?", "Удаление контакта", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                ContactsProject.Contacts.Remove(ContactsProject.Contacts[ContactsListBox.SelectedIndex]);
+                CreateContactList();
+                ProjectManager.SaveToFile(ContactsProject);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var aboutForm = new AboutProgramForm();
+            aboutForm.ShowDialog();
+        }
+
+        /// <summary>
+        /// Изменение выделения текущего контакта
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContactsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ContactsListBox.SelectedItem != null)
+            {
+                string surnameListBox = (string)ContactsListBox.SelectedItem;
+                List<Contact> contacts = new List<Contact>();
+                contacts = ContactsProject.Contacts;
+                foreach (var contact in contacts)
+                {
+                    string contactValue = contact.Surname + " " + contact.Name;
+                    if (surnameListBox == contactValue)
+                    {
+                        NameTextBox.Text = contact.Name;
+                        SurnameTextBox.Text = contact.Surname;
+                        EmailTextBox.Text = contact.Email;
+                        VKTextBox.Text = contact.IdVK;
+                        BirthdayDateTime.Text = contact.BirthDate.ToString("d");
+                        PhoneTextBox.Text = contact.ContactNumber.Number.ToString();
+
+                        ContactsProject.CurrentContact = contact;
+                        ProjectManager.SaveToFile(ContactsProject);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сохранение всех контактов при выходе из программы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContactsAppForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ProjectManager.SaveToFile(ContactsProject);
+        }
+
+        private void FindTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (FindTextBox.Text == "")
+            {
+                CreateContactList();
+            }
+            else
+            {
+                RecreateContactList(FindTextBox.Text);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProjectManager.SaveToFile(ContactsProject);
+            Close();
+        }
+    }
+}
